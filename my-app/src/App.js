78c17a4510884e5bcfa5d51cd65d9627ad2107dc
Musab-Al-omari <musabsomari@gmail.com>
2021-06-13 20:42:39 +0300

    new

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Header from './Header.js'
+import Footer from './Footer.js';
+import Main from './Main.js';
+import './App.css';
+
+function App() {
+
+
+
+
+
+
+  return (
+    <React.Fragment>
+      <Header />
+      <Main />
+      
+      <Footer />
+    </React.Fragment>);
+}
+
+export default App;
---
+++ my-app/src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.scss'
+
+const Footer = () => {
+    return (
+        <footer>
+            <h4>
+                &copy; Musab al omari website 6/13/2021
+            </h4>
+        </footer>
+    )
+}
+
+export  default Footer;
